225192be5d1e42b1a97dad74c1268831340683d8
Mike Palmer <mike.palmer@energage.com>
2018-09-06 19:30:37 -0400

    Add styles for file upload section

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
-import includes from 'lodash/includes'
 import styled from 'react-emotion'
-import ParseMapWorker from './parse-map.worker'
 import GetStarted from './GetStarted'
 import Map from './Map'
 import './App.css'
 
 const SUPPORTED_FILE_TYPES = ['text/html', 'image/svg+xml']
 
-const Root = styled.div`
-    ${tw`h-full w-full`};
-`
+const Root = styled.div(tw`h-full w-full bg-grey-lighter`)
 
 class App extends React.Component {
     state = {
-        count: 0,
-        selectedFile: null,
         mapData: null,
         displayedLabel: null
     }
 
-    componentDidMount() {
-        this.worker = new ParseMapWorker()
-        this.worker.addEventListener('message', event => this.setState({ mapData: event.data }))
-    }
-
-    fileTypeIsSupported() {
-        return includes(SUPPORTED_FILE_TYPES, this.state.selectedFile.type)
-    }
-
-    handleFileChange = event => {
-        this.setState({ selectedFile: event.target.files[0] })
-    }
-
-    handleProcessFile = () => {
-        if (!this.state.selectedFile) {
-            return
-        }
-
-        if (!this.fileTypeIsSupported()) {
-            this.setState({ error: `Unsupported file type "${this.state.selectedFile.type}"` })
-        }
-
-        const reader = new FileReader()
-        reader.onload = event => {
-            this.worker.postMessage(event.target.result)
-        }
-        reader.readAsText(this.state.selectedFile)
+    handleProcessFile = mapData => {
+        this.setState({ mapData })
     }
 
     handleSetDisplayedLabel = label => {
@@ -55,15 +24,7 @@ class App extends React.Component {
     }
 
     render() {
-        return (
-            <Root>
-                {/*<input type="file" accept={SUPPORTED_FILE_TYPES.join(',')} onChange={this.handleFileChange} />*/}
-                {/*<button disabled={!this.state.selectedFile} onClick={this.handleProcessFile}>*/}
-                {/*Process file*/}
-                {/*</button>*/}
-                {this.state.mapData ? this.renderMap() : this.renderGetStarted()}
-            </Root>
-        )
+        return <Root>{this.state.mapData ? this.renderMap() : this.renderGetStarted()}</Root>
     }
 
     renderGetStarted() {
--- src/GetStarted/index.js
+++ src/GetStarted/index.js
@@ -1,37 +1,142 @@
 import React from 'react'
-import styled, { css } from 'react-emotion'
+import PropTypes from 'prop-types'
+import styled, { css, cx, keyframes } from 'react-emotion'
 import Dropzone from 'react-dropzone'
+import Colors from '../config/colors'
+import ParseMapWorker from '../parse-map.worker'
+import FileIcon from './FileIcon'
 
-const Container = styled.div(tw`max-w-sm m-auto py-16`)
-const Card = styled.div(tw`border rounded shadow-md text-center p-8`)
-const Title = styled.h2(tw`font-light`)
+const Container = styled.div(tw`py-16`)
+const Card = styled.div(tw`w-128 mx-auto border rounded-lg shadow-lg text-center bg-white`)
+const Title = styled.h2(tw`font-light text-4xl mb-2 mt-4`)
+const Subtitle = styled.span(tw`font-light text-lg`)
+const Hidden = styled.div(props => (props.if ? tw`hidden` : {}))
+const Error = styled.div`
+    ${props => (props.active ? null : tw`hidden`)};
+    ${tw`p-4 leading-normal text-lg`};
+`
 
-const dropzone = css`
-    ${tw`text-center cursor-pointer`};
+const dropzone = css(tw`text-center cursor-pointer p-16`)
 
-    .status {
-        ${tw`h-32 w-32 border border-1 rounded-full inline-block mb-4`};
+const loadingAnimation = keyframes`
+    from {
+        transform: scale(1);
+    }
+    to {
+        transform: scale(0.6);
     }
 `
 
+// prettier-ignore
+const statusIndicator = css`
+    ${tw`h-64 w-64 relative rounded-full inline-block mb-4 inline-flex items-center justify-center`}
+    box-shadow: 0 0 0 4px ${Colors['grey-light']};
+    transition: box-shadow 400ms;
+    
+    > svg {
+        transition: opacity 400ms;
+    }
+`
+const statusIndicatorActive = css`
+    box-shadow: 0 0 0 4px ${Colors['green-light']}, inset 0 0 64px ${Colors['green-light']};
+
+    > svg {
+        opacity: 0.25;
+    }
+`
+const statusIndicatorLoading = css`
+    box-shadow: none;
+
+    &:after {
+        ${tw`absolute pin rounded-full`};
+        content: '';
+        box-shadow: 0 0 48px 2px ${Colors['green-light']}, inset 0 0 48px 2px ${Colors['green-light']};
+        animation: ${loadingAnimation} 1s ease-in-out infinite alternate;
+    }
+
+    > svg {
+        opacity: 0;
+    }
+`
+const statusIndicatorError = css`
+    box-shadow: 0 0 0 4px ${Colors['red-light']};
+`
+
 class GetStarted extends React.Component {
+    static propTypes = {
+        supportedFileTypes: PropTypes.arrayOf(PropTypes.string).isRequired,
+        onSubmitFile: PropTypes.func.isRequired
+    }
+
     state = {
-        selectedFile: null
+        loading: false,
+        error: null
+    }
+
+    componentDidMount() {
+        this.worker = new ParseMapWorker()
+        this.worker.addEventListener('message', event => this.props.onSubmitFile(event.data))
+        this.worker.onerror = event => {
+            this.setState({ loading: false, error: true })
+            event.preventDefault()
+        }
+    }
+
+    handleResetError = () => {
+        this.setState({ error: false })
     }
 
     handleDrop = files => {
-        this.setState({ selectedFile: files[0] })
+        if (files.length === 0) {
+            return
+        }
+
+        this.setState({ loading: true })
+        const reader = new FileReader()
+        reader.onload = event => {
+            this.worker.postMessage(event.target.result)
+        }
+        reader.readAsText(files[0])
     }
 
     render() {
         return (
             <Container>
-                <Dropzone className={dropzone} disablePreview accept={this.props.supportedFileTypes}>
-                    <Card>
-                        <div className="status" />
-                        <Title>{'Drop your file or click to browse'}</Title>
-                    </Card>
-                </Dropzone>
+                <Card>
+                    <Dropzone
+                        className={dropzone}
+                        disablePreview
+                        accept={this.props.supportedFileTypes}
+                        onDragEnter={this.handleResetError}
+                        onDrop={this.handleDrop}>
+                        {({ isDragAccept }) => (
+                            <React.Fragment>
+                                <div
+                                    className={cx(statusIndicator, {
+                                        [statusIndicatorActive]: isDragAccept,
+                                        [statusIndicatorLoading]: this.state.loading,
+                                        [statusIndicatorError]: this.state.error
+                                    })}>
+                                    <Error active={this.state.error}>
+                                        {`There was an error processing the file.
+                                        That file type may not be supported yet.`}
+                                    </Error>
+                                    <Hidden if={this.state.error}>
+                                        <FileIcon />
+                                    </Hidden>
+                                </div>
+                                <Hidden if={this.state.loading}>
+                                    <Title>{'Drag your file here'}</Title>
+                                    <Subtitle>{'or click to browse'}</Subtitle>
+                                </Hidden>
+                                <Hidden if={!this.state.loading}>
+                                    <Title>{'Generating map...'}</Title>
+                                    <Subtitle>{'This could take a moment'}</Subtitle>
+                                </Hidden>
+                            </React.Fragment>
+                        )}
+                    </Dropzone>
+                </Card>
             </Container>
         )
     }
