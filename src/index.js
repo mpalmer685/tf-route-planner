5800c9f09b358601b9169f03549d70acf2011b68
Mike Palmer <mike.palmer@energage.com>
2018-09-12 09:32:12 -0400

    Add rematch persist and localForage to persist redux state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { getPersistor } from '@rematch/persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
 import './index.css'
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
+const persistor = getPersistor()
+
+ReactDOM.render(
+    <PersistGate persistor={persistor}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </PersistGate>,
+    document.getElementById('root')
+)
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import { init } from '@rematch/core'
 import selectPlugin from '@rematch/select'
+import createPersistPlugin from '@rematch/persist'
+import localForage from 'localforage'
 import * as models from './models'
+
+const persistPlugin = createPersistPlugin({
+    storage: localForage,
+    version: 1,
+    whitelist: ['mapData', 'settings', 'lines']
+})
 
 export const store = init({
     models,
-    plugins: [selectPlugin()]
+    plugins: [selectPlugin(), persistPlugin]
 })
 
 export const { select } = store
